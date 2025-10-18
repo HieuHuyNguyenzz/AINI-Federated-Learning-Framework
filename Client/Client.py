403ec7aa55b14bc3863c8b598f56f6fb94d52fdd
Nguyễn Huy Hiệu <huyhieu1422002@gmail.com>
2025-10-18 11:06:46 +0700

    update some functions

--- Client/Client.py
+++ Client/Client.py
@@ -1,12 +1,19 @@
-
+from utils import get_parameters, set_parameters
 
 class Client:
-    def __init__(self, name, client_id, trainset, testset):
+    def __init__(self, name, client_id, model, trainset, testset):
         self.name = name
-        self.client_id = client_id  
+        self.client_id = client_id
+        self.model = model  
+        self.trainset = trainset
+        self.testset = testset
 
-    def local_train(self):
-        pass
 
-    def local_test(self):
-        pass
+    def local_train(self, train, config):
+        model, results = train(self.trainset, config)
+        return get_parameters(model), results
+
+
+    def local_test(self, test, config):
+        results = test(self.testset, config)
+        return results
--- Server/Server.py
+++ Server/Server.py
@@ -1,17 +1,46 @@
 import random
+from flwr.common import (
+    NDArrays,
+)
+import numpy as np
+from functools import reduce
+
 
 class Server:
-    def __init__(self, clients):
+    def __init__(self, clients, aggregation=None):
         self.clients = clients
+        self.global_model = None
+        if aggregation is None:
+            self.aggregation = self.models_aggregation
 
     def sample_clients(self, fraction_fit, num_clients):
-        
+        clients_id = [client.client_id for client in self.clients]
+        num_sampled_clients = min(int(fraction_fit * num_clients), 1)
+        sampled_clients_id = random.sample(clients_id, num_sampled_clients)
+        return sampled_clients_id
 
-    def train(self):
-        pass
+    def train(self, num_rounds, fraction_fit):
+        for round in range(num_rounds):
+            sampled_clients_id = self.sample_clients(fraction_fit, len(self.clients))
+            
+            results = [self.clients[client_id].local_train() for client_id in sampled_clients_id]
+
+            self.global_model = self.aggregation(results)
+
+
     
-    def models_aggregation(self):
-        pass
+    def models_aggregation(results: list[tuple[NDArrays, float]]) -> NDArrays:
+        num_examples_total = sum(num_examples for (_, num_examples) in results)
+
+        weighted_weights = [
+            [layer * num_examples for layer in weights] for weights, num_examples in results
+        ]
+
+        weights_prime: NDArrays = [
+            reduce(np.add, layer_updates) / num_examples_total
+            for layer_updates in zip(*weighted_weights)
+        ]
+        return weights_prime
 
     def server_test(self):
         pass
